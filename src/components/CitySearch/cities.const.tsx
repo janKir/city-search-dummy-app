92057172bf79eaeb840b4b1acca250b2c34f86c5
Jan Kirchner <jan.kirchner@envite.de>
2025-07-14 22:26:54 +0200

    150 more cities from all over the world

--- src/components/CitySearch/cities.const.tsx
+++ src/components/CitySearch/cities.const.tsx
@@ -450,5 +450,905 @@ export const cities: City[] = [
     longitude: 24.1052,
     inhabitants: 632000,
     state: 'Latvia'
+  },
+  {
+    id: 51,
+    name: 'Tokyo',
+    zipCode: '100-0001',
+    latitude: 35.6895,
+    longitude: 139.6917,
+    inhabitants: 13929286,
+    state: 'Japan'
+  },
+  {
+    id: 52,
+    name: 'Shanghai',
+    zipCode: '200000',
+    latitude: 31.2304,
+    longitude: 121.4737,
+    inhabitants: 24183300,
+    state: 'China'
+  },
+  {
+    id: 53,
+    name: 'Beijing',
+    zipCode: '100000',
+    latitude: 39.9042,
+    longitude: 116.4074,
+    inhabitants: 21542000,
+    state: 'China'
+  },
+  {
+    id: 54,
+    name: 'Delhi',
+    zipCode: '110001',
+    latitude: 28.6139,
+    longitude: 77.209,
+    inhabitants: 16787941,
+    state: 'India'
+  },
+  {
+    id: 55,
+    name: 'Mumbai',
+    zipCode: '400001',
+    latitude: 19.076,
+    longitude: 72.8777,
+    inhabitants: 12442373,
+    state: 'India'
+  },
+  {
+    id: 56,
+    name: 'São Paulo',
+    zipCode: '01000-000',
+    latitude: -23.5505,
+    longitude: -46.6333,
+    inhabitants: 12252023,
+    state: 'Brazil'
+  },
+  {
+    id: 57,
+    name: 'Rio de Janeiro',
+    zipCode: '20000-000',
+    latitude: -22.9068,
+    longitude: -43.1729,
+    inhabitants: 6748000,
+    state: 'Brazil'
+  },
+  {
+    id: 58,
+    name: 'Buenos Aires',
+    zipCode: 'C1000',
+    latitude: -34.6037,
+    longitude: -58.3816,
+    inhabitants: 2890151,
+    state: 'Argentina'
+  },
+  {
+    id: 59,
+    name: 'Lima',
+    zipCode: '15001',
+    latitude: -12.0464,
+    longitude: -77.0428,
+    inhabitants: 9674755,
+    state: 'Peru'
+  },
+  {
+    id: 60,
+    name: 'Santiago',
+    zipCode: '8320000',
+    latitude: -33.4489,
+    longitude: -70.6693,
+    inhabitants: 5743719,
+    state: 'Chile'
+  },
+  {
+    id: 61,
+    name: 'Bogotá',
+    zipCode: '110111',
+    latitude: 4.711,
+    longitude: -74.0721,
+    inhabitants: 7743955,
+    state: 'Colombia'
+  },
+  {
+    id: 62,
+    name: 'Caracas',
+    zipCode: '1010',
+    latitude: 10.4806,
+    longitude: -66.9036,
+    inhabitants: 1943901,
+    state: 'Venezuela'
+  },
+  {
+    id: 63,
+    name: 'Mexico City',
+    zipCode: '01000',
+    latitude: 19.4326,
+    longitude: -99.1332,
+    inhabitants: 9209944,
+    state: 'Mexico'
+  },
+  {
+    id: 64,
+    name: 'Toronto',
+    zipCode: 'M5H',
+    latitude: 43.6532,
+    longitude: -79.3832,
+    inhabitants: 2731571,
+    state: 'Canada'
+  },
+  {
+    id: 65,
+    name: 'Vancouver',
+    zipCode: 'V5K',
+    latitude: 49.2827,
+    longitude: -123.1207,
+    inhabitants: 631486,
+    state: 'Canada'
+  },
+  {
+    id: 66,
+    name: 'Montreal',
+    zipCode: 'H1A',
+    latitude: 45.5017,
+    longitude: -73.5673,
+    inhabitants: 1704694,
+    state: 'Canada'
+  },
+  {
+    id: 67,
+    name: 'Sydney',
+    zipCode: '2000',
+    latitude: -33.8688,
+    longitude: 151.2093,
+    inhabitants: 5312163,
+    state: 'Australia'
+  },
+  {
+    id: 68,
+    name: 'Melbourne',
+    zipCode: '3000',
+    latitude: -37.8136,
+    longitude: 144.9631,
+    inhabitants: 5078193,
+    state: 'Australia'
+  },
+  {
+    id: 69,
+    name: 'Brisbane',
+    zipCode: '4000',
+    latitude: -27.4698,
+    longitude: 153.0251,
+    inhabitants: 2514184,
+    state: 'Australia'
+  },
+  {
+    id: 70,
+    name: 'Perth',
+    zipCode: '6000',
+    latitude: -31.9505,
+    longitude: 115.8605,
+    inhabitants: 2059484,
+    state: 'Australia'
+  },
+  {
+    id: 71,
+    name: 'Auckland',
+    zipCode: '1010',
+    latitude: -36.8485,
+    longitude: 174.7633,
+    inhabitants: 1657000,
+    state: 'New Zealand'
+  },
+  {
+    id: 72,
+    name: 'Wellington',
+    zipCode: '6011',
+    latitude: -41.2865,
+    longitude: 174.7762,
+    inhabitants: 215100,
+    state: 'New Zealand'
+  },
+  {
+    id: 73,
+    name: 'Cape Town',
+    zipCode: '8000',
+    latitude: -33.9249,
+    longitude: 18.4241,
+    inhabitants: 433688,
+    state: 'South Africa'
+  },
+  {
+    id: 74,
+    name: 'Johannesburg',
+    zipCode: '2000',
+    latitude: -26.2041,
+    longitude: 28.0473,
+    inhabitants: 957441,
+    state: 'South Africa'
+  },
+  {
+    id: 75,
+    name: 'Nairobi',
+    zipCode: '00100',
+    latitude: -1.2921,
+    longitude: 36.8219,
+    inhabitants: 4397073,
+    state: 'Kenya'
+  },
+  {
+    id: 76,
+    name: 'Cairo',
+    zipCode: '11511',
+    latitude: 30.0444,
+    longitude: 31.2357,
+    inhabitants: 10230350,
+    state: 'Egypt'
+  },
+  {
+    id: 77,
+    name: 'Istanbul',
+    zipCode: '34000',
+    latitude: 41.0082,
+    longitude: 28.9784,
+    inhabitants: 15462452,
+    state: 'Turkey'
+  },
+  {
+    id: 78,
+    name: 'Ankara',
+    zipCode: '06000',
+    latitude: 39.9334,
+    longitude: 32.8597,
+    inhabitants: 5503985,
+    state: 'Turkey'
+  },
+  {
+    id: 79,
+    name: 'Moscow',
+    zipCode: '101000',
+    latitude: 55.7558,
+    longitude: 37.6173,
+    inhabitants: 11920000,
+    state: 'Russia'
+  },
+  {
+    id: 80,
+    name: 'Saint Petersburg',
+    zipCode: '190000',
+    latitude: 59.9343,
+    longitude: 30.3351,
+    inhabitants: 5384342,
+    state: 'Russia'
+  },
+  {
+    id: 81,
+    name: 'Dubai',
+    zipCode: '00000',
+    latitude: 25.2048,
+    longitude: 55.2708,
+    inhabitants: 3331420,
+    state: 'United Arab Emirates'
+  },
+  {
+    id: 82,
+    name: 'Abu Dhabi',
+    zipCode: '00000',
+    latitude: 24.4539,
+    longitude: 54.3773,
+    inhabitants: 1482816,
+    state: 'United Arab Emirates'
+  },
+  {
+    id: 83,
+    name: 'Doha',
+    zipCode: '00000',
+    latitude: 25.2854,
+    longitude: 51.531,
+    inhabitants: 956457,
+    state: 'Qatar'
+  },
+  {
+    id: 84,
+    name: 'Riyadh',
+    zipCode: '11564',
+    latitude: 24.7136,
+    longitude: 46.6753,
+    inhabitants: 6881000,
+    state: 'Saudi Arabia'
+  },
+  {
+    id: 85,
+    name: 'Jeddah',
+    zipCode: '21577',
+    latitude: 21.4858,
+    longitude: 39.1925,
+    inhabitants: 3976000,
+    state: 'Saudi Arabia'
+  },
+  {
+    id: 86,
+    name: 'Tehran',
+    zipCode: '10000',
+    latitude: 35.6892,
+    longitude: 51.389,
+    inhabitants: 8846782,
+    state: 'Iran'
+  },
+  {
+    id: 87,
+    name: 'Baghdad',
+    zipCode: '10001',
+    latitude: 33.3152,
+    longitude: 44.3661,
+    inhabitants: 7180889,
+    state: 'Iraq'
+  },
+  {
+    id: 88,
+    name: 'Karachi',
+    zipCode: '74200',
+    latitude: 24.8607,
+    longitude: 67.0011,
+    inhabitants: 14910352,
+    state: 'Pakistan'
+  },
+  {
+    id: 89,
+    name: 'Lagos',
+    zipCode: '100001',
+    latitude: 6.5244,
+    longitude: 3.3792,
+    inhabitants: 14862000,
+    state: 'Nigeria'
+  },
+  {
+    id: 90,
+    name: 'Kinshasa',
+    zipCode: '00000',
+    latitude: -4.4419,
+    longitude: 15.2663,
+    inhabitants: 14500000,
+    state: 'DR Congo'
+  },
+  {
+    id: 91,
+    name: 'Johor Bahru',
+    zipCode: '80000',
+    latitude: 1.4927,
+    longitude: 103.7414,
+    inhabitants: 858000,
+    state: 'Malaysia'
+  },
+  {
+    id: 92,
+    name: 'Bangkok',
+    zipCode: '10100',
+    latitude: 13.7563,
+    longitude: 100.5018,
+    inhabitants: 10539000,
+    state: 'Thailand'
+  },
+  {
+    id: 93,
+    name: 'Jakarta',
+    zipCode: '10110',
+    latitude: -6.2088,
+    longitude: 106.8456,
+    inhabitants: 10770487,
+    state: 'Indonesia'
+  },
+  {
+    id: 94,
+    name: 'Manila',
+    zipCode: '1000',
+    latitude: 14.5995,
+    longitude: 120.9842,
+    inhabitants: 13923452,
+    state: 'Philippines'
+  },
+  {
+    id: 95,
+    name: 'Hanoi',
+    zipCode: '100000',
+    latitude: 21.0285,
+    longitude: 105.8542,
+    inhabitants: 8000000,
+    state: 'Vietnam'
+  },
+  {
+    id: 96,
+    name: 'Ho Chi Minh City',
+    zipCode: '700000',
+    latitude: 10.7769,
+    longitude: 106.7009,
+    inhabitants: 8424000,
+    state: 'Vietnam'
+  },
+  {
+    id: 97,
+    name: 'Seoul',
+    zipCode: '04524',
+    latitude: 37.5665,
+    longitude: 126.978,
+    inhabitants: 9776000,
+    state: 'South Korea'
+  },
+  {
+    id: 98,
+    name: 'Busan',
+    zipCode: '600-011',
+    latitude: 35.1796,
+    longitude: 129.0756,
+    inhabitants: 3448737,
+    state: 'South Korea'
+  },
+  {
+    id: 99,
+    name: 'Hong Kong',
+    zipCode: '00000',
+    latitude: 22.3193,
+    longitude: 114.1694,
+    inhabitants: 7451000,
+    state: 'Hong Kong'
+  },
+  {
+    id: 100,
+    name: 'Singapore',
+    zipCode: '018989',
+    latitude: 1.3521,
+    longitude: 103.8198,
+    inhabitants: 5638700,
+    state: 'Singapore'
+  },
+  {
+    id: 101,
+    name: 'Kuala Lumpur',
+    zipCode: '50000',
+    latitude: 3.139,
+    longitude: 101.6869,
+    inhabitants: 1800000,
+    state: 'Malaysia'
+  },
+  {
+    id: 102,
+    name: 'Yangon',
+    zipCode: '11181',
+    latitude: 16.8409,
+    longitude: 96.1735,
+    inhabitants: 5214000,
+    state: 'Myanmar'
+  },
+  {
+    id: 103,
+    name: 'Dhaka',
+    zipCode: '1000',
+    latitude: 23.8103,
+    longitude: 90.4125,
+    inhabitants: 8906039,
+    state: 'Bangladesh'
+  },
+  {
+    id: 104,
+    name: 'Kathmandu',
+    zipCode: '44600',
+    latitude: 27.7172,
+    longitude: 85.324,
+    inhabitants: 1400000,
+    state: 'Nepal'
+  },
+  {
+    id: 105,
+    name: 'Colombo',
+    zipCode: '00100',
+    latitude: 6.9271,
+    longitude: 79.8612,
+    inhabitants: 561314,
+    state: 'Sri Lanka'
+  },
+  {
+    id: 106,
+    name: 'Islamabad',
+    zipCode: '44000',
+    latitude: 33.6844,
+    longitude: 73.0479,
+    inhabitants: 1095064,
+    state: 'Pakistan'
+  },
+  {
+    id: 107,
+    name: 'Kabul',
+    zipCode: '1001',
+    latitude: 34.5553,
+    longitude: 69.2075,
+    inhabitants: 4273156,
+    state: 'Afghanistan'
+  },
+  {
+    id: 108,
+    name: 'Tashkent',
+    zipCode: '100000',
+    latitude: 41.2995,
+    longitude: 69.2401,
+    inhabitants: 2546000,
+    state: 'Uzbekistan'
+  },
+  {
+    id: 109,
+    name: 'Astana',
+    zipCode: '010000',
+    latitude: 51.1694,
+    longitude: 71.4491,
+    inhabitants: 1078362,
+    state: 'Kazakhstan'
+  },
+  {
+    id: 110,
+    name: 'Baku',
+    zipCode: '1000',
+    latitude: 40.4093,
+    longitude: 49.8671,
+    inhabitants: 2230000,
+    state: 'Azerbaijan'
+  },
+  {
+    id: 111,
+    name: 'Yerevan',
+    zipCode: '0001',
+    latitude: 40.1792,
+    longitude: 44.4991,
+    inhabitants: 1075800,
+    state: 'Armenia'
+  },
+  {
+    id: 112,
+    name: 'Tbilisi',
+    zipCode: '0105',
+    latitude: 41.7151,
+    longitude: 44.8271,
+    inhabitants: 1150000,
+    state: 'Georgia'
+  },
+  {
+    id: 113,
+    name: 'Rabat',
+    zipCode: '10000',
+    latitude: 34.0209,
+    longitude: -6.8416,
+    inhabitants: 577827,
+    state: 'Morocco'
+  },
+  {
+    id: 114,
+    name: 'Casablanca',
+    zipCode: '20000',
+    latitude: 33.5731,
+    longitude: -7.5898,
+    inhabitants: 3359818,
+    state: 'Morocco'
+  },
+  {
+    id: 115,
+    name: 'Algiers',
+    zipCode: '16000',
+    latitude: 36.7538,
+    longitude: 3.0588,
+    inhabitants: 3415811,
+    state: 'Algeria'
+  },
+  {
+    id: 116,
+    name: 'Tunis',
+    zipCode: '1000',
+    latitude: 36.8065,
+    longitude: 10.1815,
+    inhabitants: 1056247,
+    state: 'Tunisia'
+  },
+  {
+    id: 117,
+    name: 'Tripoli',
+    zipCode: '1000',
+    latitude: 32.8872,
+    longitude: 13.1913,
+    inhabitants: 1150989,
+    state: 'Libya'
+  },
+  {
+    id: 118,
+    name: 'Khartoum',
+    zipCode: '11111',
+    latitude: 15.5007,
+    longitude: 32.5599,
+    inhabitants: 5259800,
+    state: 'Sudan'
+  },
+  {
+    id: 119,
+    name: 'Addis Ababa',
+    zipCode: '1000',
+    latitude: 9.03,
+    longitude: 38.74,
+    inhabitants: 3041002,
+    state: 'Ethiopia'
+  },
+  {
+    id: 120,
+    name: 'Accra',
+    zipCode: '00233',
+    latitude: 5.6037,
+    longitude: -0.187,
+    inhabitants: 2291352,
+    state: 'Ghana'
+  },
+  {
+    id: 121,
+    name: 'Dakar',
+    zipCode: '10000',
+    latitude: 14.6928,
+    longitude: -17.4467,
+    inhabitants: 1146053,
+    state: 'Senegal'
+  },
+  {
+    id: 122,
+    name: 'Abidjan',
+    zipCode: '10000',
+    latitude: 5.3453,
+    longitude: -4.0244,
+    inhabitants: 4765000,
+    state: 'Ivory Coast'
+  },
+  {
+    id: 123,
+    name: 'Kampala',
+    zipCode: '256',
+    latitude: 0.3476,
+    longitude: 32.5825,
+    inhabitants: 1507080,
+    state: 'Uganda'
+  },
+  {
+    id: 124,
+    name: 'Harare',
+    zipCode: '00263',
+    latitude: -17.8252,
+    longitude: 31.0335,
+    inhabitants: 1486000,
+    state: 'Zimbabwe'
+  },
+  {
+    id: 125,
+    name: 'Luanda',
+    zipCode: '1000',
+    latitude: -8.839,
+    longitude: 13.2894,
+    inhabitants: 8417000,
+    state: 'Angola'
+  },
+  {
+    id: 126,
+    name: 'Maputo',
+    zipCode: '1100',
+    latitude: -25.9692,
+    longitude: 32.5732,
+    inhabitants: 1200000,
+    state: 'Mozambique'
+  },
+  {
+    id: 127,
+    name: 'Antananarivo',
+    zipCode: '101',
+    latitude: -18.8792,
+    longitude: 47.5079,
+    inhabitants: 1391433,
+    state: 'Madagascar'
+  },
+  {
+    id: 128,
+    name: 'Canberra',
+    zipCode: '2600',
+    latitude: -35.2809,
+    longitude: 149.13,
+    inhabitants: 462000,
+    state: 'Australia'
+  },
+  {
+    id: 129,
+    name: 'Ottawa',
+    zipCode: 'K1A',
+    latitude: 45.4215,
+    longitude: -75.6997,
+    inhabitants: 934243,
+    state: 'Canada'
+  },
+  {
+    id: 130,
+    name: 'Edmonton',
+    zipCode: 'T5J',
+    latitude: 53.5461,
+    longitude: -113.4938,
+    inhabitants: 981280,
+    state: 'Canada'
+  },
+  {
+    id: 131,
+    name: 'Calgary',
+    zipCode: 'T2P',
+    latitude: 51.0447,
+    longitude: -114.0719,
+    inhabitants: 1239220,
+    state: 'Canada'
+  },
+  {
+    id: 132,
+    name: 'Quebec City',
+    zipCode: 'G1A',
+    latitude: 46.8139,
+    longitude: -71.2082,
+    inhabitants: 542298,
+    state: 'Canada'
+  },
+  {
+    id: 133,
+    name: 'Halifax',
+    zipCode: 'B3J',
+    latitude: 44.6488,
+    longitude: -63.5752,
+    inhabitants: 403131,
+    state: 'Canada'
+  },
+  {
+    id: 134,
+    name: 'Winnipeg',
+    zipCode: 'R3C',
+    latitude: 49.8951,
+    longitude: -97.1384,
+    inhabitants: 749534,
+    state: 'Canada'
+  },
+  {
+    id: 135,
+    name: 'St. John’s',
+    zipCode: 'A1C',
+    latitude: 47.5615,
+    longitude: -52.7126,
+    inhabitants: 108860,
+    state: 'Canada'
+  },
+  {
+    id: 136,
+    name: 'Victoria',
+    zipCode: 'V8W',
+    latitude: 48.4284,
+    longitude: -123.3656,
+    inhabitants: 85792,
+    state: 'Canada'
+  },
+  {
+    id: 137,
+    name: 'Recife',
+    zipCode: '50000-000',
+    latitude: -8.0476,
+    longitude: -34.877,
+    inhabitants: 1645727,
+    state: 'Brazil'
+  },
+  {
+    id: 138,
+    name: 'Fortaleza',
+    zipCode: '60000-000',
+    latitude: -3.7172,
+    longitude: -38.5431,
+    inhabitants: 2686612,
+    state: 'Brazil'
+  },
+  {
+    id: 139,
+    name: 'Salvador',
+    zipCode: '40000-000',
+    latitude: -12.9777,
+    longitude: -38.5016,
+    inhabitants: 2886698,
+    state: 'Brazil'
+  },
+  {
+    id: 140,
+    name: 'Brasília',
+    zipCode: '70000-000',
+    latitude: -15.7939,
+    longitude: -47.8828,
+    inhabitants: 3055149,
+    state: 'Brazil'
+  },
+  {
+    id: 141,
+    name: 'Curitiba',
+    zipCode: '80000-000',
+    latitude: -25.4284,
+    longitude: -49.2733,
+    inhabitants: 1963726,
+    state: 'Brazil'
+  },
+  {
+    id: 142,
+    name: 'Porto Alegre',
+    zipCode: '90000-000',
+    latitude: -30.0346,
+    longitude: -51.2177,
+    inhabitants: 1484941,
+    state: 'Brazil'
+  },
+  {
+    id: 143,
+    name: 'Belo Horizonte',
+    zipCode: '30000-000',
+    latitude: -19.9167,
+    longitude: -43.9345,
+    inhabitants: 2521564,
+    state: 'Brazil'
+  },
+  {
+    id: 144,
+    name: 'Goiania',
+    zipCode: '74000-000',
+    latitude: -16.6869,
+    longitude: -49.2648,
+    inhabitants: 1536097,
+    state: 'Brazil'
+  },
+  {
+    id: 145,
+    name: 'Campinas',
+    zipCode: '13000-000',
+    latitude: -22.9099,
+    longitude: -47.0626,
+    inhabitants: 1213792,
+    state: 'Brazil'
+  },
+  {
+    id: 146,
+    name: 'Belém',
+    zipCode: '66000-000',
+    latitude: -1.4558,
+    longitude: -48.4902,
+    inhabitants: 1499641,
+    state: 'Brazil'
+  },
+  {
+    id: 147,
+    name: 'Manaus',
+    zipCode: '69000-000',
+    latitude: -3.119,
+    longitude: -60.0217,
+    inhabitants: 2182763,
+    state: 'Brazil'
+  },
+  {
+    id: 148,
+    name: 'Porto',
+    zipCode: '4000-001',
+    latitude: 41.1579,
+    longitude: -8.6291,
+    inhabitants: 214349,
+    state: 'Portugal'
+  },
+  {
+    id: 149,
+    name: 'Lisbon',
+    zipCode: '1100-001',
+    latitude: 38.7223,
+    longitude: -9.1393,
+    inhabitants: 545000,
+    state: 'Portugal'
+  },
+  {
+    id: 150,
+    name: 'Madrid',
+    zipCode: '28001',
+    latitude: 40.4168,
+    longitude: -3.7038,
+    inhabitants: 3223000,
+    state: 'Spain'
   }
 ]
